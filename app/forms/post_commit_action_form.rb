616aad8abacf16a85d9f102588f3d42c8b502db8
lisichkin <lisichkin@elins.ru>
2015-07-20 17:11:19 +0300

    deleted redundant parts

--- app/forms/post_commit_action_form.rb
+++ app/forms/post_commit_action_form.rb
@@ -1,17 +1,10 @@
 class PostCommitActionForm < Reform::Form
   include Reform::Form::ModelReflections
-  include Reform::Form::Lotus
 
-  property :action_type, virtual: true
-  property :description
-  property :url
-  property :repository_id
   property :condition_group,
            prepopulator: :prepopulate_condition_group!,
            populate_if_empty: PostCommitAction::ConditionGroup,
            form: PostCommitAction::ConditionGroupForm
-
-  delegates :validator, :validators_on
 
   private
 
